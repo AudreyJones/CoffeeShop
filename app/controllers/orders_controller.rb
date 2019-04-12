5b80e590f415cf8325d8cd45e5671fb7c981eca1
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-12 20:05:46 +0000

    Done.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,32 @@
 class OrdersController < ApplicationController
 
   get "/orders/new_order" do
+    @drinks = Drink.all
     erb :"orders/new_order"
   end
 
   post "/order" do
+    binding.pry
     @user = User.find_by_id(session[:user_id])
     @order = Order.create(user_id: session[:user_id], favorite: params[:favorite])
-    @your_drinks = []
-    for params.each do |key, value|
-      if key == "cappuccino" || key == "latte" || key == "hot coffee" || key == "frozen coffee" || key == "iced coffee"
-        drink = Drink.create(drink_type: key)
-        @your_drinks << drink
-        @orderdrink = OrderDrink.create(order_id: @order.id, drink_id: drink.id)
-      end
-    @orderdrink
-    @your_drinks
+    @drinks = params[:drinks]
+    @drinks.each do |drink|
+      #make an order to get order id
+      #make orderdrinks that all link to that order
+      
+      Order.new()
+      Orderdrink.new()
     end
+    # for params.each do |key, value|
+    #   if key == "cappuccino" || key == "latte" || key == "hot coffee" || key == "frozen coffee" || key == "iced coffee"
+    #     drink = Drink.create(drink_type: key)
+    #     @your_drinks << drink
+    #     @orderdrink = OrderDrink.create(order_id: @order.id, drink_id: drink.id)
+    #   end
+    # @orderdrink
+    # @your_drinks
+    # endDr
     # @drink.modifications = params[:mods]  ###Needs to be linked to the drink it is for!!!
-    # binding.pry
-    # OrderDrink.find_by_id()
     erb :"/users/homepage"
   end
 
@@ -30,6 +37,10 @@ class OrdersController < ApplicationController
 
   get "/orders/edit_order" do
     erb :"/orders/edit_order"
+  end
+
+  get "/orders/:id" do
+    erb :"orders/show_order"
   end
 
 end
