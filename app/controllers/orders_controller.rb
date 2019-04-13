5d49a205b124a1228803c0931759ae1ae5dfc08a
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-13 16:09:55 +0000

    increased to 10 drinks in seed.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,12 @@
 class OrdersController < ApplicationController
 
   get "/orders/new_order" do
+    binding.pry
     @drinks = Drink.all
     erb :"orders/new_order"
   end
 
   post "/order" do
-    binding.pry
     @user = User.find_by_id(session[:user_id])
     @order = Order.create(user_id: session[:user_id], favorite: params[:favorite])
     @drinks = params[:drinks]
@@ -16,7 +16,7 @@ class OrdersController < ApplicationController
       # Order.new()
       # Orderdrink.new()
       this_drink = Drink.find_by_id(drink)
-      @order_drinks = OrderDrink.new(order_id: @order.id ,drink_id: drink)
+      order_drink = OrderDrink.new(order_id: @order.id ,drink_id: drink)
 
     end
     # for params.each do |key, value|
